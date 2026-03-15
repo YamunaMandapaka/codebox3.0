51b3b3bd12966c65e6721dfbaa86cd7f34563dbd
YamunaMandapaka <n221095@rguktn.ac.in>
2026-03-15 15:13:45 +0530

    Intial Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <h1>
+      <h1 className="font-game text-2xl">
         Welcome to the home page
       </h1>
+      <Button>Subscribe</Button>
     </div>
   );
 }
